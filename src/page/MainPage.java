7fed80bb2e2b6ed52cbc0d5778e414a4e7385e2f
guoys <renner861@sina.com>
2015-12-01 18:08:26 +0800

    finish main page method

--- src/page/MainPage.java
+++ src/page/MainPage.java
@@ -128,7 +128,165 @@ public class MainPage extends UiAutomatorTestCase implements mainPageConstants{
 	public String stateBtnText(){
 		return mainpage.getText(STATEBTN);
 	}
-    //判断当天工作工作状态是否存在
+    //判断当前工作状态是否存在
+	public Boolean stateBtnExist(){
+		return mainpage.isExist(STATEBTN);		
+	}
+	//点击接单设置按钮
+	public void setBtnClick(){
+		try {
+			mainpage.getObj(SETBTN).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取接单设置文本
+	public String setBtnText(){
+		return mainpage.getText(SETBTN);
+	}
+    //判断接单设置是否存在
+	public Boolean setBtnExist(){
+		return mainpage.isExist(SETBTN);		
+	}
+	//点击切换状态按钮
+	public void changeBtnClick(){
+		try {
+			mainpage.getObj(CHANGEBTN).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取切换状态文本
+	public String changeBtnText(){
+		return mainpage.getText(CHANGEBTN);
+	}
+    //判断切换状态是否存在
+	public Boolean changeBtnExist(){
+		return mainpage.isExist(CHANGEBTN);		
+	}
+	
+	//点击未完成订单数
+	public void unFinishCountClick(){
+		try {
+			mainpage.getObj(UNFINISHCOUNT).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取未完成订单数文本
+	public String unFinishCountText(){
+		return mainpage.getText(UNFINISHCOUNT);
+	}
+	   //判断未完成订单数是否存在
+	public Boolean unFinishCountExist(){
+		return mainpage.isExist(UNFINISHCOUNT);		
+	}
+	//点击订单类型
+	public void orderTypeClick(){
+		try {
+			mainpage.getObj(ORDERTYPE).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取订单类型文本
+	public String orderTypeText(){
+	     return mainpage.getText(ORDERTYPE);
+	}
+	//判断订单类型是否存在
+	public Boolean orderTypeExist(){
+		return mainpage.isExist(ORDERTYPE);		
+	}
+	//点击航班号
+	public void flightNoClick(){
+		try {
+			mainpage.getObj(FLIGHTNO).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取航班号文本
+	public String flightNoText(){
+	    return mainpage.getText(FLIGHTNO);
+	}
+	//判断航班号是否存在
+	public Boolean flightNoExist(){
+		return mainpage.isExist(FLIGHTNO);		
+	}	
+	//点击订单状态
+	public void orderStatusClick(){
+		try {
+			mainpage.getObj(ORDERSTATUS).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取订单状态文本
+	public String orderStatusText(){
+	    return mainpage.getText(ORDERSTATUS);
+	}
+	//判断订单状态是否存在
+	public Boolean orderStatusExist(){
+		return mainpage.isExist(ORDERSTATUS);		
+	}	
+	//点击使用日期
+	public void useDateClick(){
+		try {
+			mainpage.getObj(USEDATE).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取使用日期文本
+	public String useDateText(){
+	    return mainpage.getText(USEDATE);
+	}
+	//判断使用日期是否存在
+	public Boolean useDateExist(){
+		return mainpage.isExist(USEDATE);		
+	}
+	//点击改派按钮
+	public void changeOrderBtnClick(){
+		try {
+			mainpage.getObj(CHANGEORDERBTN).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取改派按钮文本
+	public String changeOrderBtnText(){
+	    return mainpage.getText(CHANGEORDERBTN);
+	}
+	//判断改派按钮是否存在
+	public Boolean changeOrderBtnExist(){
+		return mainpage.isExist(CHANGEORDERBTN);		
+	}
+	//点击上车地点
+	public void upLocationClick(){
+		try {
+			mainpage.getObj(UPLOCATION).click();
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取上车地点文本
+	public String upLocationText(){
+	    return mainpage.getText(UPLOCATION);
+	}
+	//判断上车地点是否存在
+	public Boolean upLocationExist(){
+		return mainpage.isExist(UPLOCATION);		
+	}
+	
 	
 	
 	
