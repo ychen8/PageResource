c8eced90d3534371b11a3931211bc7a616fc314f
ychen8 <598016812@qq.com>
2015-12-02 13:54:10 +0800

    Revert "11"
    This reverts commit 8854a1e0f3a3c6197168808212f6fd6a6794e299.

---
+++ src/page/SetPage.java
@@ -0,0 +1,10 @@
+package page;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+import driverInterface.SetPageConstants;
+
+public class SetPage extends UiAutomatorTestCase implements SetPageConstants {
+
+	
+}
