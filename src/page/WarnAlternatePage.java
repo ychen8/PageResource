418f8e0d8574ac750be8968202f6ddb60c95b9ef
guoys <renner861@sina.com>
2015-12-03 09:48:57 +0800

    d

---
+++ src/page/WarnAlternatePage.java
@@ -0,0 +1,11 @@
+package page;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+import driverInterface.WarnAlternateConstants;
+
+public class WarnAlternatePage extends UiAutomatorTestCase implements WarnAlternateConstants{
+	
+	
+
+}
