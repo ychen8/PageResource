24be6da25fc2a0479e92da27573a6d528a149855
yx.li07 <yx.li07@zuche.com>
2015-12-04 17:35:27 +0800

    新建NaviBeforeConstants，剩下的回家写~~~

--- src/driverInterface/NaviBeforeConstants.java
+++ src/driverInterface/NaviBeforeConstants.java
@@ -17,10 +17,24 @@ public interface NaviBeforeConstants {
     static String DESTINATION_BOX="com.szzc.ucar.driver:id/nav_search";
     //目的地删除按钮
     static String DESTINATION_DEL="com.szzc.ucar.driver:id/nav_del";
+    //目的地列表
+    static String DESTINATION_LIST="com.szzc.ucar.driver:id/list";
+    //目的地title
+    static String NAV_POI_TITLE="com.szzc.ucar.driver:id/nav_poi_title";
+    //目的地详情
+    static String NAV_POI_ADDR="com.szzc.ucar.driver:id/nav_poi_addr";
     //导航类型栏
     static String NAVI_TYPE_BOX="com.szzc.ucar.driver:id/scroll_view";
-    //导航类型layout
-    static String NAVI_LAYOUT="com.szzc.ucar.driver:id/navi_item_short";
+    //导航类型-不走高速
+    static String NAVI_NOEXPRESS="com.szzc.ucar.driver:id/navi_item_noexpress";
+    //导航类型-最快
+    static String NAVI_FAST="com.szzc.ucar.driver:id/navi_item_fast";
+    //导航类型-最短
+    static String NAVI_SHORT="com.szzc.ucar.driver:id/navi_item_short";
+    //导航类型-避免拥堵
+    static String NAVI_FASTTIME="com.szzc.ucar.driver:id/navi_item_fasttime";
+    //导航类型-避免收费
+    static String NAVI_SAVEMONEY="com.szzc.ucar.driver:id/navi_item_savemoney";
     //导航类型-中间字
     static String NAVI_TYPE_BIG="com.szzc.ucar.driver:id/navi_strategy_fg";
     //导航类型-上方小字
