3e85a19137252d90e306a25bdef942e0f3e515c7
yx.li07 <yx.li07@zuche.com>
2015-12-04 17:17:09 +0800

    新增NaviBeforeConstants

---
+++ src/driverInterface/NaviBeforeConstants.java
@@ -0,0 +1,34 @@
+package driverInterface;
+/**
+ * Description：开始导航页面
+ * Date: 2015-12-4
+ * @author 李怡萱
+ * */
+public interface NaviBeforeConstants {
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//开始导航页面-资源ID
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//标题栏
+	static String TITLE="com.szzc.ucar.driver:id/titlebar_title";
+    //返回按钮
+    static String BACKBTN="com.szzc.ucar.driver:id/titlebar_lefticon";
+    //目的地输入框
+    static String DESTINATION_BOX="com.szzc.ucar.driver:id/nav_search";
+    //目的地删除按钮
+    static String DESTINATION_DEL="com.szzc.ucar.driver:id/nav_del";
+    //导航类型栏
+    static String NAVI_TYPE_BOX="com.szzc.ucar.driver:id/scroll_view";
+    //导航类型layout
+    static String NAVI_LAYOUT="com.szzc.ucar.driver:id/navi_item_short";
+    //导航类型-中间字
+    static String NAVI_TYPE_BIG="com.szzc.ucar.driver:id/navi_strategy_fg";
+    //导航类型-上方小字
+    static String NAVI_TYPE_SMALL="com.szzc.ucar.driver:id/navi_strategy";
+    //导航类型-预计时间
+    static String NAVI_TIME="com.szzc.ucar.driver:id/navi_time";
+    //导航类型-预计里程
+    static String NAVI_DISTANCE="com.szzc.ucar.driver:id/navi_distance";
+    //开始导航按钮
+    static String NAVI_BTN="com.szzc.ucar.driver:id/nav_navi";
+}
