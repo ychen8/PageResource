934def0c3fb66eedab3a8770c9ae5f4c22e32ce5
guoys <renner861@sina.com>
2015-12-06 22:58:46 +0800

    订单推送页面---未完成

---
+++ src/driverInterface/OrderPushConstants.java
@@ -0,0 +1,44 @@
+package driverInterface;
+
+public interface OrderPushConstants {
+	//订单类型
+	static String ORDERTYPE="com.szzc.ucar.driver:id/order_type";
+	//订单类型角标
+	static String ORDTYPEIMG="com.szzc.ucar.driver:id/imgorder_type";
+	//关闭按钮
+	static String CLSBTN="com.szzc.ucar.driver:id/close_btn";
+	//使用时间
+	static String USEDATE="com.szzc.ucar.driver:id/use_date";
+	//用车距离
+	static String ORDDISTANCE="com.szzc.ucar.driver:id/order_distance";
+	//上车地点
+	static String UPNAME="com.szzc.ucar.driver:id/uplocation_name";
+	//上车地点详情
+	static String UPDETAIL="com.szzc.ucar.driver:id/uplocation_detail";
+	//下车地点
+	static String DOWNAME="com.szzc.ucar.driver:id/downlocation_name";
+	//下车地点详情
+	static String DOWNDETAIL="com.szzc.ucar.driver:id/downlocation_detail";
+	//上车地点TIP
+	static String UPTIP="com.szzc.ucar.driver:id/uplocation_tip";
+	//下车地点TIP
+	static String DOWNTIP="com.szzc.ucar.driver:id/downlocation_tip";
+	//特别提示信息
+	static String SPECNAME="com.szzc.ucar.driver:id/fav_name";
+	//特别提示TIP
+	static String SPECTIP="com.szzc.ucar.driver:id/fav_tip";
+	//向上滑动控件
+	static String MOVEUP="com.szzc.ucar.driver:id/lltotop";
+	//确定按钮
+	static String OKBTN="com.szzc.ucar.driver:id/accept_btn";
+	//接单设置按钮
+	static String SETBTN="com.szzc.ucar.driver:id/btnset";
+	//备注按钮
+	static String REMARK="com.szzc.ucar.driver:id/remark";
+	//备注TIP
+	static String REMARKTIP="com.szzc.ucar.driver:id/remark_tip";
+	//浮动系数
+	static String FACTOR="com.szzc.ucar.driver:id/price_factor";
+
+
+}
