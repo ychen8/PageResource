b92f21e6a29dfca9497c3e29da242ec384b06ad4
ychen8 <598016812@qq.com>
2015-12-09 17:55:57 +0800

    新增司机分布constants

---
+++ src/driverInterface/DriverDistriConstants.java
@@ -0,0 +1,26 @@
+package driverInterface;
+/**
+ *创建日期:2015-12-9
+ *作者： 陈玉婵
+ */
+public interface DriverDistriConstants {
+	//返回icon
+	static String BACK="com.szzc.ucar.driver:id/titlebar_lefticon";
+	//标题
+	static String TITLE="com.szzc.ucar.driver:id/titlebar_title";
+	//司机分布页面内容frame
+	static String CONTENT="com.szzc.ucar.driver:id/content";
+	//放大－index
+	static int INDEX_ENLARGE=0;
+	//缩小－index
+	static int INDEX_NARROW=1;
+	//重新定位－index
+	static int INDEX_REPOSITION=2;
+	//看全城－index
+	static int INDEX_WHOLE_CITY=3;
+	//底部静态价格显示栏－index
+	static int INDEX_STATIC_PRICE=4;
+	
+	
+
+}
