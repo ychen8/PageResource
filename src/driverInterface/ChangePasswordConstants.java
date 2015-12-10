0b368d8d0110c42bda8b7bfb09ea76a71b9ffb01
ychen8 <598016812@qq.com>
2015-12-10 18:02:39 +0800

    新增修改密码constants

---
+++ src/driverInterface/ChangePasswordConstants.java
@@ -0,0 +1,17 @@
+package driverInterface;
+/**
+ *创建日期:2015-12-10
+ *作者： 陈玉婵
+ */
+public interface ChangePasswordConstants {
+	//返回icon
+	static String BACK="com.szzc.ucar.driver:id/titlebar_lefticon";
+	//标题
+	static String TITLE="com.szzc.ucar.driver:id/titlebar_title";
+	static String PW_CUR="com.szzc.ucar.driver:id/pw_cur";
+	static String PW_NEW="com.szzc.ucar.driver:id/pw_new";
+	static String TIP="com.szzc.ucar.driver:id/tip";
+	static String SHOW_CK="com.szzc.ucar.driver:id/show_ck";
+	static String PW_MODFIY_OK="com.szzc.ucar.driver:id/pw_modfiy_ok";
+
+}
