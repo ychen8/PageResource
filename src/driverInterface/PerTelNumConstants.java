afd65c2160def7b753d813880a7921bc6b05b2ad
XTB <XTB@XTB-PC>
2015-12-10 13:43:44 +0800

    新增私人号码页--完成

---
+++ src/driverInterface/PerTelNumConstants.java
@@ -0,0 +1,19 @@
+package driverInterface;
+
+public interface PerTelNumConstants {
+
+	//私人电话标题
+	static String PERTELTITLE="com.szzc.ucar.driver:id/titlebar_title";
+	//返回按钮
+	static String BACKBTN="com.szzc.ucar.driver:id/titlebar_lefticon";
+	//电话输入框
+	static String NUMINPUTBLANK="com.szzc.ucar.driver:id/phone";
+	//提示信息
+	static String TIPINFO="com.szzc.ucar.driver:id/text";
+	//保存按钮
+	static String SAVEBTN="com.szzc.ucar.driver:id/save";
+	//提示页错误信息
+	static String ERRINFO="com.szzc.ucar.driver:id/confirm_text";
+	//提示页确定按钮
+	static String OKBTN="com.szzc.ucar.driver:id/ok_btn";
+}
