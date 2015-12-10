cd12d872c303d311c69806ec241d7422cc3b3af0
ychen8 <598016812@qq.com>
2015-12-10 17:18:09 +0800

    新增personsetconstants

---
+++ src/driverInterface/PersonSetConstants.java
@@ -0,0 +1,37 @@
+package driverInterface;
+/**
+ *创建日期:2015-12-10
+ *作者： 陈玉婵
+ */
+public interface PersonSetConstants {
+	//返回icon
+	static String BACK="com.szzc.ucar.driver:id/titlebar_lefticon";
+	//标题
+	static String TITLE="com.szzc.ucar.driver:id/titlebar_title";
+	//修改密码　　
+	static String LL_MODIFY_PASS="com.szzc.ucar.driver:id/ll_modify_pass";
+	//修改密码　　
+	static int INDEX_LL_MODIFY_PASS=0;
+	//导航下载栏
+	static String LL_OFFLINE_MAP="com.szzc.ucar.driver:id/ll_offline_map";
+	//导航下载栏文本－index
+	static int INDEX_LL_OFFLINE_MAP=0;
+	//意见反馈栏
+	static String LL_FEEDBACK="com.szzc.ucar.driver:id/ll_feedback";
+	//意见反馈栏文本－index
+	static int INDEX_LL_FEEDBACK=0;
+	//使用指南栏
+	static String LL_USE_GUIDE="com.szzc.ucar.driver:id/ll_use_guide";
+	//使用指南栏文本－index
+	static int INDEX_LL_USE_GUIDE=0;
+	//检测新版本栏
+	static String LL_CHECK_UPDATE="com.szzc.ucar.driver:id/ll_check_update";
+	//检测新版本栏文本－index
+	static int INDEX_CHECK_UPDATE=0;
+	//检测新版本栏文本值－index
+	static int INDEX_CHECK_UPDATE_VALUE=1;
+	//退出登录
+	static String BTN_LOGOUT="com.szzc.ucar.driver:id/btn_logout";
+
+
+}
