ca6af6fd57147c363c7477eeb63b5bb0cb69d3a8
yx.li07 <yx.li07@zuche.com>
2015-12-10 14:08:56 +0800

    新增订单列表页

---
+++ src/driverInterface/OrderListConstants.java
@@ -0,0 +1,60 @@
+package driverInterface;
+/**
+ * Description：订单列表
+ * Date: 2015-12-10
+ * @author 李怡萱
+ * */
+public interface OrderListConstants {
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//订单列表-资源ID
+	//////////////////////////////////////////////////////////////////////////////////////////////////
+	//标题栏
+	static String TITLE="com.szzc.ucar.driver:id/titlebar_title";
+	//返回按钮
+	static String BACKBTN="com.szzc.ucar.driver:id/back_btn";
+	//查询
+	static String QUERYBTN="com.szzc.ucar.driver:id/titlebar_righttext";
+	//类型切换
+	static String TAB_BAR="com.szzc.ucar.driver:id/tabbar";
+	//未完成
+	static int NOT_FINISHED=0;
+	//已完成
+	static int FINISHED=1;
+	//tabview（近三月-无订单）
+	static String TAB_VIEW1="com.szzc.ucar.driver:id/tabview";
+	//tabview（其他时间-无订单）
+	static String TAB_VIEW2="com.szzc.ucar.driver:id/order_pull_list";
+	//无订单text-ID
+	static int NO_ORDER_ID=0;
+	//列表
+	static String ORDER_LIST="android:id/list";
+	//订单类型
+	static String ORDER_TYPE="com.szzc.ucar.driver:id/txt_order_type";
+	//航班号
+	static String FLIGHT_NO="com.szzc.ucar.driver:id/txt_flightno";
+	//订单金额
+	static String ORDER_PRICE="com.szzc.ucar.driver:id/txt_order_price";
+	//订单状态
+	static String ORDER_STATE="com.szzc.ucar.driver:id/order_state";
+	//订单号
+	static String ORDER_NO="com.szzc.ucar.driver:id/txt_orderid";
+    //订单日期
+    static String ORDER_DATE="com.szzc.ucar.driver:id/use_date";
+    //改派按钮
+    static String CHANGE_ORDER_BTN="com.szzc.ucar.driver:id/btn_change_order";
+    //上车地点
+    static String UP_LOC="com.szzc.ucar.driver:id/uplocation_name";
+    //上车地点详情
+    static String UP_LOC_DETAIL="com.szzc.ucar.driver:id/uplocation_detail_name";
+    //下车地点
+    static String DOWN_LOC="com.szzc.ucar.driver:id/downlocation_name";
+    //下车地点详情
+    static String DOWN_LOC_DETAIL="com.szzc.ucar.driver:id/downlocation_detail_name";
+    
+
+	
+
+
+
+
+}
