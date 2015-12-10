d4375e17ef31fe8d038a7344d0e9621182134189
yx.li07 <yx.li07@zuche.com>
2015-12-10 11:13:39 +0800

    修正部分类调用方法错误（isExist->isExistFromId）

--- src/page/ChargeDetailPage.java
+++ src/page/ChargeDetailPage.java
@@ -31,19 +31,19 @@ public class ChargeDetailPage extends UiAutomatorTestCase implements ChargeDetai
 	}
 	//判断支付状态-文本是否存在
 	public Boolean payStatusTextExist(){
-		return chargedetailpage.isExist(VIEW_CLASS, PAY_STATUS_TEXT_ID);
+		return chargedetailpage.isExistFromClass(VIEW_CLASS, PAY_STATUS_TEXT_ID);
 	}
 	//获取支付状态-文本
 	public String payStatusTextText(){
-	    return chargedetailpage.getText(VIEW_CLASS, PAY_STATUS_TEXT_ID);
+	    return chargedetailpage.getTextFromClass(VIEW_CLASS, PAY_STATUS_TEXT_ID);
 	}
 	//判断支付状态是否存在
 	public Boolean payStatusExist(){
-		return chargedetailpage.isExist(VIEW_CLASS, PAY_STATUS_ID);
+		return chargedetailpage.isExistFromClass(VIEW_CLASS, PAY_STATUS_ID);
 	}
 	//获取支付状态-文本
 	public String payStatusText(){
-	    return chargedetailpage.getText(VIEW_CLASS, PAY_STATUS_ID);
+	    return chargedetailpage.getTextFromClass(VIEW_CLASS, PAY_STATUS_ID);
 	}
 	//判断总金额是否存在
 	public Boolean moneyAmountExist(){
@@ -55,11 +55,11 @@ public class ChargeDetailPage extends UiAutomatorTestCase implements ChargeDetai
 	}
 	//判断元-文本是否存在
 	public Boolean yuanExist(){
-		return chargedetailpage.isExist(VIEW_CLASS,YUAN_ID);
+		return chargedetailpage.isExistFromClass(VIEW_CLASS,YUAN_ID);
 	}
 	//获取元-文本
 	public String yuanText(){
-	    return chargedetailpage.getText(VIEW_CLASS,YUAN_ID);
+	    return chargedetailpage.getTextFromClass(VIEW_CLASS,YUAN_ID);
 	}
 	//判断明细列表是否存在
 	public Boolean costListExist(){
@@ -71,19 +71,19 @@ public class ChargeDetailPage extends UiAutomatorTestCase implements ChargeDetai
 	}
 	//判断费用项名称是否存在(index为偶数)
 	public Boolean costNameExist(int index){
-		return chargedetailpage.isExist(COST_LIST, index);
+		return chargedetailpage.isExistFromId(COST_LIST, index);
 	}
 	//获取费用项名称
 	public String costNameText(int index){
-		return chargedetailpage.getText(COST_LIST, index);
+		return chargedetailpage.getTextFromId(COST_LIST, index);
 	}
 	//判断费用项数值是否存在(index为奇数)
 	public Boolean costValueExist(int index){
-		return chargedetailpage.isExist(COST_LIST, index);
+		return chargedetailpage.isExistFromId(COST_LIST, index);
 	}
 	//获取费用项数值
 	public String costValueText(int index){
-		return chargedetailpage.getText(COST_LIST, index);
+		return chargedetailpage.getTextFromId(COST_LIST, index);
 	}
 	
 }
