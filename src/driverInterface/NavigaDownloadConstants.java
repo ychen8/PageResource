6ae3709440c8d274617b828724fc69ebeaebb45a
ychen8 <598016812@qq.com>
2015-12-11 16:11:12 +0800

    新增导航下载

---
+++ src/driverInterface/NavigaDownloadConstants.java
@@ -0,0 +1,42 @@
+package driverInterface;
+/**
+ *创建日期:2015-12-11
+ *作者： 陈玉婵
+ */
+public interface NavigaDownloadConstants {
+	//返回icon
+	static String BACK="com.szzc.ucar.driver:id/back_btn";
+	//全部、已下载tab
+	static String MAP_TYPE_CHECKBOX="com.szzc.ucar.driver:id/map_type_checkbox";
+	//城市输入框
+	static String SEARCH="com.szzc.ucar.driver:id/search";
+	//城市输入框-输入值
+	static String SEARCH_VALUE="长春";
+	
+	//城市列表
+	static String LISTVIEW="com.szzc.ucar.driver:id/listView";
+
+	//城市值
+	static String CITY_VALUE="com.szzc.ucar.driver:id/label";
+	//城市下载进度
+	static String DOWNLOAD_PROCESS="com.szzc.ucar.driver:id/download_process";
+	//城市下载完成label
+	static String LABEL_DOWNLOAD_COMPLETE="com.szzc.ucar.driver:id/label_download_complete";
+	//当前城市-index
+	static int INDEX_CURREN_CITY=0;
+	//当前城市值－index
+	static int INDEX_CURREN_CITY_VALUE=1;
+	//热门城市－index
+	static int INDEX_HOT_CITY=2;
+	static int INDEX_HOT_CITY1=3;
+	static int INDEX_HOT_CITY2=4;
+	static int INDEX_HOT_CITY3=5;
+	static int INDEX_HOT_CITY4=6;
+	static int INDEX_HOT_CITY5=7;
+	static int INDEX_HOT_CITY6=8;
+	static int INDEX_HOT_CITY7=9;
+	
+	
+
+
+}
---
+++ src/page/NavigaDownloadPage.java
@@ -0,0 +1,73 @@
+package page;
+
+import baseMethod.MainMethod;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+import driverInterface.NavigaDownloadConstants;
+
+/**
+ *创建日期:2015-12-11
+ *作者： 陈玉婵
+ */
+public class NavigaDownloadPage extends UiAutomatorTestCase implements
+		NavigaDownloadConstants {
+	MainMethod navigaDownloadPage=new MainMethod();
+	//判断返回icon是否存在
+	public Boolean returnExist(){
+		return navigaDownloadPage.isExist(BACK);
+	}
+	//点击返回
+	public void returnClick(){
+		navigaDownloadPage.clickObj(BACK);
+	}
+	//判断全部、已下载tab是否存在
+	public Boolean mapTypeExist(){
+		return navigaDownloadPage.isExist(MAP_TYPE_CHECKBOX);
+	}
+	//获取全部、已下载内容
+	public String mapTypeText(){
+		return navigaDownloadPage.getText(MAP_TYPE_CHECKBOX);
+	}
+	//点击全部、已下载tab
+	public void mapTypeClick(){
+		navigaDownloadPage.clickObj(MAP_TYPE_CHECKBOX);
+	}
+	//判断城市输入框是否存在
+	public Boolean searchExist(){
+		return navigaDownloadPage.isExist(SEARCH);
+	}
+	//清空城市输入框内容
+	public void searchClear(){
+		navigaDownloadPage.cleartxt(SEARCH);
+	}
+	//输入城市
+	public void searchSet(){
+		navigaDownloadPage.setxt(SEARCH, SEARCH_VALUE);
+	}
+	//判断城市值是否存在
+	public Boolean CityValueExist(int index){
+		return navigaDownloadPage.isExistFromList(LISTVIEW, index,CITY_VALUE);
+	}
+	//获取城市值文本
+	public String cityeValueText(int index){
+		return navigaDownloadPage.getText(LISTVIEW, index,CITY_VALUE);
+	}
+	//判断城市下载进度是否存在
+	public Boolean downloadProcessExist(int index){
+		return navigaDownloadPage.isExistFromList(LISTVIEW, index,DOWNLOAD_PROCESS);
+	}
+	//获取城市下载进度文本
+	public String downloadProcessText(int index){
+		return navigaDownloadPage.getText(LISTVIEW, index,DOWNLOAD_PROCESS);
+	}
+	//判断城市下载完成label是否存在
+	public Boolean downloadCompleExist(int index){
+		return navigaDownloadPage.isExistFromList(LISTVIEW, index,LABEL_DOWNLOAD_COMPLETE);
+	}
+	//获取城市下载完成label文本
+	public String downloadCompleText(int index){
+		return navigaDownloadPage.getText(LISTVIEW, index,LABEL_DOWNLOAD_COMPLETE);
+	}
+
+}
