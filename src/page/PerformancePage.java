33ee850e567b0c7992a8cd8bd744e5d13baf617b
yx.li07 <yx.li07@zuche.com>
2016-01-04 11:48:33 +0800

    各个绩效元素 改为分别由index获取

--- src/page/PerformancePage.java
+++ src/page/PerformancePage.java
@@ -32,14 +32,23 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean rlWorkTimeExist(){
 		return performancePage.isExist(RL_WORK_TIME);
 	}
-	//判断平均工作时长栏的标题或值是否存在
-	public Boolean workTimeExist(int index){
-		return performancePage.isExistFromId(RL_WORK_TIME, index);
+	//判断平均工作时长栏的标题是否存在
+	public Boolean workTimeExist(){
+		return performancePage.isExistFromId(RL_WORK_TIME, INDEX_WORK_TIME);
 		
 	}
-	//获取平均工作时长栏的标题或值
-	public String workTimeText(int index){
-		return performancePage.getTextFromId(RL_WORK_TIME, index);
+	//获取平均工作时长栏的标题
+	public String workTimeText(){
+		return performancePage.getTextFromId(RL_WORK_TIME, INDEX_WORK_TIME);
+	}
+	//判断平均工作时长栏的值是否存在
+	public Boolean workTimeValueExist(){
+		return performancePage.isExistFromId(RL_WORK_TIME, INDEX_WORK_TIME_VALUE);
+		
+	}
+	//获取平均工作时长栏的值
+	public String workTimeValueText(){
+		return performancePage.getTextFromId(RL_WORK_TIME, INDEX_WORK_TIME_VALUE);
 	}
 	//点击平均工作时长栏
 	public void rlWorkTimeClick(){
@@ -50,14 +59,29 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean rlWorkDaysExist(){
 		return performancePage.isExist(RL_WORK_DAYS);
 	}
-	//判断工作天数栏的标题或值是否存在
-	public Boolean workDaysExist(int index){
-		return performancePage.isExistFromId(RL_WORK_DAYS, index);
-		
+	//判断工作天数栏的标题是否存在
+	public Boolean workDaysExist(){
+		return performancePage.isExistFromId(RL_WORK_DAYS, INDEX_WORK_DAYS);		
 	}
-	//获取工作天数栏的标题或值
-	public String workDaysText(int index){
-		return performancePage.getTextFromId(RL_WORK_DAYS, index);
+	//获取工作天数栏的标题
+	public String workDaysText(){
+		return performancePage.getTextFromId(RL_WORK_DAYS, INDEX_WORK_DAYS);
+	}
+	//判断工作天数栏的值是否存在
+	public Boolean workDaysValueExist(){
+		return performancePage.isExistFromId(RL_WORK_DAYS, INDEX_WORK_DAYS_VALUE);		
+	}
+	//获取工作天数栏的值
+	public String workDaysValueText(){
+		return performancePage.getTextFromId(RL_WORK_DAYS, INDEX_WORK_DAYS_VALUE);
+	}
+	//判断应出勤天数是否存在
+	public Boolean wholeDaysValueExist(){
+		return performancePage.isExistFromId(RL_WORK_DAYS, INDEX_WHOLE_DAYS_VALUE);
+	}
+	//获取应出勤天数的值
+	public String wholeDaysValueText(){
+		return performancePage.getTextFromId(RL_WORK_DAYS, INDEX_WHOLE_DAYS_VALUE);
 	}
 	//点击工作天数栏
 	public void rlWorkDaysClick(){
@@ -68,14 +92,21 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean rlRspRateExist(){
 		return performancePage.isExist(RL_RSP_RATE);
 	}
-	//判断响应率栏的标题或值或总天数是否存在
-	public Boolean rspRateExist(int index){
-		return performancePage.isExistFromId(RL_RSP_RATE, index);
-		
+	//判断响应率栏的标题是否存在
+	public Boolean rspRateExist(){
+		return performancePage.isExistFromId(RL_RSP_RATE, INDEX_RSP_RATE);		
 	}
-	//获取响应率栏的标题或值或总天数
-	public String rspRateText(int index){
-		return performancePage.getTextFromId(RL_RSP_RATE, index);
+	//获取响应率栏的标题
+	public String rspRateText(){
+		return performancePage.getTextFromId(RL_RSP_RATE, INDEX_RSP_RATE);
+	}
+	//判断响应率栏的值是否存在
+	public Boolean rspRateValueExist(){
+		return performancePage.isExistFromId(RL_RSP_RATE, INDEX_RSP_RATE_VALUE);		
+	}
+	//获取响应率栏的值
+	public String rspRateValueText(){
+		return performancePage.getTextFromId(RL_RSP_RATE, INDEX_RSP_RATE_VALUE);
 	}
 	//点击响应率栏
 	public void rlRspRateClick(){
@@ -86,14 +117,21 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean rlSvcScoreExist(){
 		return performancePage.isExist(RL_SVC_SCORE);
 	}
-	//判断服务评分栏的标题或值是否存在
-	public Boolean svcScoreExist(int index){
-		return performancePage.isExistFromId(RL_SVC_SCORE, index);
-		
+	//判断服务评分栏的标题是否存在
+	public Boolean svcScoreExist(){
+		return performancePage.isExistFromId(RL_SVC_SCORE, INDEX_SVC_SCORE);	
 	}
-	//获取服务评分栏的标题或值
-	public String svcScoreText(int index){
-		return performancePage.getTextFromId(RL_SVC_SCORE, index);
+	//获取服务评分栏的标题
+	public String svcScoreText(){
+		return performancePage.getTextFromId(RL_SVC_SCORE, INDEX_SVC_SCORE);
+	}
+	//判断服务评分栏的值是否存在
+	public Boolean svcScoreValueExist(){
+		return performancePage.isExistFromId(RL_SVC_SCORE, INDEX_SVC_SCORE_VALUE);	
+	}
+	//获取服务评分栏的值
+	public String svcScoreValueText(){
+		return performancePage.getTextFromId(RL_SVC_SCORE, INDEX_SVC_SCORE_VALUE);
 	}
 	//点击服务评分栏
 	public void rlSvcScoreClick(){
@@ -104,14 +142,21 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean llOrderQuantityExist(){
 		return performancePage.isExist(LL_ORDER_QUANTITY);
 	}
-	//判断订单量栏的标题或值是否存在
-	public Boolean orderQuantityExist(int index){
-		return performancePage.isExistFromId(LL_ORDER_QUANTITY, index);
-		
+	//判断订单量栏的标题是否存在
+	public Boolean orderQuantityExist(){
+		return performancePage.isExistFromId(LL_ORDER_QUANTITY, INDEX_ORDER_QUANTITY);		
 	}
-	//获取订单量栏的标题或值
-	public String orderQuantityText(int index){
-		return performancePage.getTextFromId(LL_ORDER_QUANTITY, index);
+	//获取订单量栏的标题
+	public String orderQuantityText(){
+		return performancePage.getTextFromId(LL_ORDER_QUANTITY, INDEX_ORDER_QUANTITY);
+	}
+	//判断订单量栏的值是否存在
+	public Boolean orderQuantityValueExist(){
+		return performancePage.isExistFromId(LL_ORDER_QUANTITY, INDEX_ORDER_QUANTITY_VALUE);		
+	}
+	//获取订单量栏的值
+	public String orderQuantityValueText(){
+		return performancePage.getTextFromId(LL_ORDER_QUANTITY, INDEX_ORDER_QUANTITY_VALUE);
 	}
 	//点击订单量栏
 	public void llOrderQuantityClick(){
@@ -122,14 +167,21 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean llRechargeAmountExist(){
 		return performancePage.isExist(LL_RECHARGE_AMOUNT);
 	}
-	//判断客户充值额栏的标题或值是否存在
-	public Boolean rechargeAmountExist(int index){
-		return performancePage.isExistFromId(LL_RECHARGE_AMOUNT, index);
-		
+	//判断客户充值额栏的标题是否存在
+	public Boolean rechargeAmountExist(){
+		return performancePage.isExistFromId(LL_RECHARGE_AMOUNT, INDEX_RECHARGE_AMOUNT);		
 	}
-	//获取客户充值额栏的标题或值
-	public String rechargeAmountText(int index){
-		return performancePage.getTextFromId(LL_RECHARGE_AMOUNT, index);
+	//获取客户充值额栏的标题
+	public String rechargeAmountText(){
+		return performancePage.getTextFromId(LL_RECHARGE_AMOUNT, INDEX_RECHARGE_AMOUNT);
+	}
+	//判断客户充值额栏的值是否存在
+	public Boolean rechargeAmountValueExist(){
+		return performancePage.isExistFromId(LL_RECHARGE_AMOUNT, INDEX_RECHARGE_AMOUNT_VALUE);		
+	}
+	//获取客户充值额栏的值
+	public String rechargeAmountValueText(){
+		return performancePage.getTextFromId(LL_RECHARGE_AMOUNT, INDEX_RECHARGE_AMOUNT_VALUE);
 	}
 	//点击客户充值额栏
 	public void llRechargeAmountClick(){
@@ -140,14 +192,21 @@ public class PerformancePage extends UiAutomatorTestCase implements PerformanceC
 	public Boolean llTurnoverExist(){
 		return performancePage.isExist(LL_TURNOVER);
 	}
-	//判断计奖营业额栏的标题或值是否存在
-	public Boolean turnoverExist(int index){
-		return performancePage.isExistFromId(LL_TURNOVER, index);
-		
+	//判断计奖营业额栏的标题是否存在
+	public Boolean turnoverExist(){
+		return performancePage.isExistFromId(LL_TURNOVER, INDEX_TURNOVER);		
 	}
-	//获取计奖营业额栏的标题或值
-	public String turnoverText(int index){
-		return performancePage.getTextFromId(LL_TURNOVER, index);
+	//获取计奖营业额栏的标题
+	public String turnoverText(){
+		return performancePage.getTextFromId(LL_TURNOVER, INDEX_TURNOVER);
+	}
+	//判断计奖营业额栏的值是否存在
+	public Boolean turnoverValueExist(){
+		return performancePage.isExistFromId(LL_TURNOVER, INDEX_TURNOVER_VALUE);		
+	}
+	//获取计奖营业额栏的值
+	public String turnoverValueText(){
+		return performancePage.getTextFromId(LL_TURNOVER, INDEX_TURNOVER_VALUE);
 	}
 	//点击计奖营业额栏
 	public void llTurnoverClick(){
